8f9591bda0d29e3dfcd16258f71ff977020e4913
SebastianSerranoB <sebastianserranob11@gmail.com>
2023-11-22 17:25:47 -0300

    Y la base de datos?

--- MiProyectoDePrueba/Program.cs
+++ MiProyectoDePrueba/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World! im not sebastian");
+            Console.WriteLine("Hello, World! im nots sebastian");
         }
     }
 }
